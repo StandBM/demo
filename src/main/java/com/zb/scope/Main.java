61717cf5f8ed4b86d0d986efa0c507a2ac29f04b
liujin <2075314032@qq.com>
2018-06-08 14:24:55 +0800

    首次提交

--- src/main/java/com/zb/scope/Main.java
+++ src/main/java/com/zb/scope/Main.java
@@ -3,8 +3,18 @@ package com.zb.scope;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
-    public static void main(String[] args){
+      public static void main(String[] args){
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ScopeConfig.class);
+
+        DemoSingletonService s1 = context.getBean(DemoSingletonService.class);
+        DemoSingletonService s2 = context.getBean(DemoSingletonService.class);
+
+        DemoPrototypeService p1 = context.getBean(DemoPrototypeService.class);
+        DemoPrototypeService p2 = context.getBean(DemoPrototypeService.class);
+
+        System.out.print("s1与s2是否相等"+s1.equals(s2));
+        System.out.print("p1与p2是否相等"+p1.equals(p2));
+        context.close();
     }
 }
 
